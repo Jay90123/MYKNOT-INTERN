095b8d2527b1fb009c23ea3c974e786666f9218f
Leston <lestonair6574@gmail.com>
2022-10-21 14:18:30 +0530

    #3 ready for uploading

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary=require("cloudinary")
 
 cloudinary.config({ 
-    cloud_name: 'dn9cofvdv', 
-    api_key: '912278242495112', 
-    api_secret: 'v1TTxBjJa3A6gVAZiJMazvrYT7I' 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET
   });
 
 
