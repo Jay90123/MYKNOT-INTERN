9080c146bd2b4b3c5f59087de6f652f9fb682de5
Sumit <jagtasumit668@gmail.com>
2023-11-08 14:37:32 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(
       "https://myknot-intern-kuvc.vercel.app/",
       "http://localhost:3000/login",
       "http://localhost:3000",
+      "http://65.0.19.30:5000"
     ],
     credentials: true,
   })
